bc33ad9bb2f308a7389d9890b85fd3fe8c0bf2ae
MichalinaSobkiewicz <sobkiewiczmichalina@gmail.com>
2023-04-05 17:00:03 +0200

    logarytmy

---
+++ lab2_/próba.py
@@ -0,0 +1,14 @@
+import math
+wynik= math.log(3)
+wynik1=math.log(3,0.5)`
+
+wynik4= math.log(20)
+wynik5=math.log(20,math.e)
+
+wynik2= math.log(3,8)
+print(wynik)
+print(wynik1)
+print(wynik2)
+
+print(wynik4)
+print(wynik5)
